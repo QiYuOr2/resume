1dbe0b575f9fd4a64ff01921c3c808013ce1c3bc
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2024-12-05 01:24:39 +0800

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,11 +5,6 @@ export default defineNuxtConfig({
   app: {
     baseURL: "/resume/",
   },
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, "dist"),
-    },
-  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/icon"],
