7b63ab4105f84091fd82039b65d8eb78fdaed5ae
qiyuor2 <48339849+QiYuOr2@users.noreply.github.com>
2024-12-05 01:41:41 +0800

    config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,8 @@
-import path from "node:path";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
     baseURL: "/resume/",
   },
-  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxt/content", "@nuxt/icon"],
 });
